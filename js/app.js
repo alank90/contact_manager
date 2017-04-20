af3f197b4bb03d595839cb05bcea6c4d3349a7b9
Alan <akillian@outlook.com>
2017-04-20 13:51:41 -0400

    Commit

--- js/app.js
+++ js/app.js
@@ -14,8 +14,57 @@
 
 } (jQuery));
 
+/* ============ Backbone.js Below This Line ===============*/
+//  Model Declaration
 var Contact = Backbone.Model.extend  ({
     defaults: {
         photo: "/img/placeholder.png"
     }
-})
+});
+
+//  Collection Declaration
+var Directory = Backbone.Collection.extend({
+    model: Contact
+});
+
+/* ===== Views Declaration. Both Individual Model and Collection Views. ======= */
+//   View for each indivual Model.
+var ContactView = Backbone.View.extend ({
+    tagName: "article",
+    className: "contact-container",
+    template: $("#contactTemplate").html(),
+
+    render: function() {
+        var tmpl = _.template(this.template);
+
+        this.$el.html(tmpl(this.model.toJson()));
+        return this;
+    }
+});
+
+// Note This is a comment.
+//View for Collection
+var DirectoryView = Backbone.View.extend ({
+    el: $("#contacts"),
+
+    initialize: function () {
+        this.collection = new Directory(contacts);
+        this.render();
+    },
+
+    render: function () {
+        var that = this;
+        console.log("This is that: " + that);
+        _.each(this.collection.models, function (item) {
+            that.renderContact(item);
+        }, this);
+    },
+
+    renderContact: function (item) {
+        var contactView = new ContactView({
+            model: item
+        });
+        this.$el.append(contactView.render().el);
+    }
+});
+
